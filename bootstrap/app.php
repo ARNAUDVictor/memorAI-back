2d3f7aea06ca6a125b1d6f45ba013e767f95ff12
Paul Delifer <paul.delifer@yahoo.fr>
2024-12-12 22:32:49 +0100

    chore: cors 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias('cors', HandleCors::class);
-        $middleware->pushGlobalMiddleware('cors');
+        $middleware->pushMiddleware(HandleCors::class); 
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
